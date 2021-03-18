487cd40eb9cb81f29fdddf0e6ec998af8a15aa2b
Mateus Santana <mateuss.dev@gmail.com>
2021-03-18 20:34:56 -0300

    feat(pages): add index(landing) page

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import LandingLayout from 'layouts/landing'
+
+import { LandingHero } from 'ui/landing'
+
+const IndexPage = () => {
+  return (
+    <LandingLayout>
+      <LandingHero />
+    </LandingLayout>
+  )
+}
+
+export default IndexPage
