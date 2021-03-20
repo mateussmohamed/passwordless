a4b4955382435a4f7f7409b35fa95a7240268da5
Mateus Santana <mateuss.dev@gmail.com>
2021-03-19 21:35:31 -0300

    feat(lib): add withAuth to use in protected pages

---
+++ src/lib/with-auth.ts
@@ -0,0 +1,21 @@
+import { GetServerSidePropsContext } from 'next'
+
+import { getSession } from 'next-auth/client'
+
+export default function withAuth() {
+  return async (context: GetServerSidePropsContext) => {
+    const session = await getSession(context)
+
+    if (!session) {
+      return {
+        props: {},
+        redirect: {
+          destination: '/auth/signin',
+          permanent: false
+        }
+      }
+    }
+
+    return { props: { session } }
+  }
+}
