3935b06c5ea420769ec190254aaef1b7e12764b8
Mateus Santana <mateuss.dev@gmail.com>
2021-03-19 21:41:57 -0300

    feat(pages/auth): add verify request

---
+++ src/pages/auth/verify-request.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+
+function AuthVerifyRequestPage() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h1 className="font-bold text-center text-2xl mb-5">
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+          </h1>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Check your email
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            A sign in link has been sent to your email address.
+          </p>
+
+          <div className="text-center w-full whitespace-nowrap mt-10">
+            <Link href="/">
+              <a className="inline-block transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-4 h-4 inline-block align-text-top"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                </svg>
+
+                <span className="inline-block ml-1">Back to home</span>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AuthVerifyRequestPage
