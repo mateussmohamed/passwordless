22549f469427d7cf3149dcefb1d0af305ac29ec2
Mateus Santana <mateuss.dev@gmail.com>
2021-03-19 23:03:36 -0300

    refactor(ui/app): change border type

--- src/ui/app/content.tsx
+++ src/ui/app/content.tsx
@@ -9,7 +9,7 @@ function AppContent({ children }: AppContentProps) {
     <main>
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
-          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 p-5">{children}</div>
+          <div className="border-2 border-solid border-gray-200 rounded-lg py-12">{children}</div>
         </div>
       </div>
     </main>
