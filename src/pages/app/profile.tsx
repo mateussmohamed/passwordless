073856d9a9a378373ba59f1d88b0f80bb011aa47
Mateus Santana <mateuss.dev@gmail.com>
2021-03-19 23:04:38 -0300

    feat(pages/app): add profile form in profile page

--- src/pages/app/profile.tsx
+++ src/pages/app/profile.tsx
@@ -2,10 +2,28 @@ import withAuth from 'lib/with-auth'
 
 import AppLayout from 'layouts/app'
 
-function ProfilePage() {
+import ProfileForm from 'components/profile-fom'
+
+function ProfilePage(props: ProtectedPageProps) {
+  const {
+    session: { user }
+  } = props
+
   return (
-    <AppLayout pageTitle="Profile Page">
-      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Profile</h2>
+    <AppLayout {...props} pageTitle="Profile Page">
+      <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+            <p className="mt-1 text-sm text-gray-600">
+              This information will be displayed publicly so be careful what you share.
+            </p>
+          </div>
+        </div>
+        <div className="mt-5 md:mt-0 md:col-span-2">
+          <ProfileForm user={user} />
+        </div>
+      </div>
     </AppLayout>
   )
 }
