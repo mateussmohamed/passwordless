74761642fb466690b6e237072d8ba2867f42188a
Mateus Santana <mateuss.dev@gmail.com>
2022-07-06 22:32:11 -0300

    chore: upgrade migration

--- prisma/migrations/20220706220309_next_auth/migration.sql
+++ prisma/migrations/20220706220309_next_auth/migration.sql
@@ -1,24 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `accounts` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `verification_requests` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "accounts";
-
--- DropTable
-DROP TABLE "sessions";
-
--- DropTable
-DROP TABLE "users";
-
--- DropTable
-DROP TABLE "verification_requests";
-
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
