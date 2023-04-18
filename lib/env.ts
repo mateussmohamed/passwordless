ebfbe339b91410475c0c6f9268ee66e04eb42ba0
Mateus Santana <mateuss.dev@gmail.com>
2023-04-18 19:17:34 -0300

    fix: env file

--- lib/env.ts
+++ lib/env.ts
@@ -1,49 +1,57 @@
+// credits: https://create.t3.gg/en/usage/env-variables
 import { z } from 'zod'
 
 const isServer = typeof window === 'undefined'
 
-const serverEnvVariables = z.object({
-  GITHUB_CLIENT_SECRET: z.string(),
-  GITHUB_CLIENT_ID: z.string(),
+const IS_PREVIEW = [
+  process.env.VERCEL_ENV,
+  process.env.NEXT_PUBLIC_VERCEL_ENV
+].includes('preview')
+
+const server = z.object({
+  GITHUB_CLIENT_SECRET: z.string().nonempty(),
+  GITHUB_CLIENT_ID: z.string().nonempty(),
   NODE_ENV: z.enum(['development', 'test', 'production'])
 })
 
-const universalVariables = z.object({
-  APP_URL: z.string().url(),
+const client = z.object({
+  APP_URL: z.string().url().nonempty(),
   APP_PREVIEW: z.boolean(),
-  APP_PREVIEW_EMAIL: z.string()
+  APP_PREVIEW_EMAIL: z.string().nonempty()
 })
 
-let env = {
-  APP_URL: process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL,
-  APP_PREVIEW: [
-    process.env.VERCEL_ENV,
-    process.env.NEXT_PUBLIC_VERCEL_ENV
-  ].includes('preview'),
-
+const processEnv = {
+  APP_URL: String(process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL),
+  APP_PREVIEW: IS_PREVIEW,
   APP_PREVIEW_EMAIL:
     process.env.APP_PREVIEW_EMAIL || process.env.NEXT_PUBLIC_APP_PREVIEW_EMAIL,
-
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-
   NODE_ENV: process.env.NODE_ENV
 }
 
-const merged = serverEnvVariables.merge(universalVariables)
+const merged = server.merge(client)
+
+type Merged = z.infer<typeof merged>
 
 const parsed = isServer
-  ? merged.safeParse(env)
-  : universalVariables.safeParse(env)
+  ? merged.safeParse(processEnv)
+  : client.safeParse(processEnv)
 
 if (parsed.success === false) {
   console.error(
     '❌ Invalid environment variables:',
-    JSON.stringify(parsed.error.flatten().fieldErrors)
+    parsed.error.flatten().fieldErrors
   )
   throw new Error('Invalid environment variables')
-} else {
-  env = parsed.data as Required<z.infer<typeof merged>>
 }
+
+const data = parsed.data as Merged
+
+const env = new Proxy(data, {
+  get(target, prop) {
+    return target[prop as keyof typeof data]
+  }
+})
 
 export { env }
